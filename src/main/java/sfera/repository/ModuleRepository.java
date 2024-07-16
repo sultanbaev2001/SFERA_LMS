ed628b92942a48d0d0cb3c0842226f3f9ec8806d
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2024-07-16 18:05:00 +0300

    modul tugadi

--- src/main/java/sfera/repository/ModuleRepository.java
+++ src/main/java/sfera/repository/ModuleRepository.java
@@ -3,6 +3,10 @@ package sfera.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sfera.entity.Module;
 
+import java.util.List;
+
 public interface ModuleRepository extends JpaRepository<Module, Integer> {
     boolean existsByOrderName(String name);
+
+    List<Module> findAllByCategory_Id(Integer categoryId);
 }
