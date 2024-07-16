06b43f39ecf1fe41984487202de96e3fa8074afc
sultanbaev2001 <sultanbaevm2001@gmail.com>
2024-07-16 09:56:21 +0500

    Security поправил

--- src/main/java/sfera/entity/User.java
+++ src/main/java/sfera/entity/User.java
@@ -4,15 +4,20 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import sfera.entity.enums.ERole;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -25,5 +30,38 @@ public class User {
     @OneToOne
     private Contact contact;
 
+    private boolean accountNonExpired;
+    private boolean credentialsNonExpired;
+    private boolean accountNonLocked;
+    private boolean enabled;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return phoneNumber;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
---
+++ src/main/java/sfera/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package sfera.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sfera.entity.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByPhoneNumber(String phoneNumber);
+}
