b6ff970413fe8f6a37490a79bc37ccfbbfa0aaaa
Ulugbek Bakhtiyorov <164327420+bakhtiyorov-306@users.noreply.github.com>
2024-07-16 16:34:12 +0500

    commit

--- src/main/java/sfera/service/StatisticService.java
+++ src/main/java/sfera/service/StatisticService.java
@@ -4,20 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import sfera.entity.Category;
-import sfera.entity.Feedback;
-import sfera.entity.User;
 import sfera.entity.enums.ERole;
 import sfera.payload.ApiResponse;
 import sfera.payload.StatisticDto;
 import sfera.payload.res.ResCategory;
-import sfera.payload.top.TopTeacherDTO;
 import sfera.repository.CategoryRepository;
-import sfera.repository.FeedbackRepository;
 import sfera.repository.GroupRepository;
 import sfera.repository.UserRepository;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +21,6 @@ public class StatisticService {
     private final UserRepository userRepository;
     private final CategoryRepository categoryRepository;
     private final GroupRepository groupRepository;
-    private final FeedbackRepository feedbackRepository;
 
     public ApiResponse getAllCount(){
         Integer teacherCount = userRepository.countByRoleAndActiveTrue(ERole.ROLE_TEACHER);
@@ -60,66 +54,6 @@ public class StatisticService {
         return new ApiResponse("Success",true,HttpStatus.OK,resCategoryList);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-//    TOP Teacher, student and group service
-
-//    Teacher
-//    public ApiResponse getTopTeacher(){
-//        Map<UUID, Double> topTeacherMap = new HashMap<>();
-//        List<TopTeacherDTO> teacherList = new ArrayList<>();
-//        List<User> activeTeachers = userRepository.findActiveTeachers();
-//        if (!activeTeachers.isEmpty()){
-//            for (User activeTeacher : activeTeachers) {
-//                double sum=0;
-//                int count = feedbackRepository.countByTeacherId(activeTeacher.getId());
-//                for (Feedback feedback : feedbackRepository.findByTeacherId(activeTeacher.getId())) {
-//                    sum+=feedback.getRate();
-//                }
-//                TopTeacherDTO topTeacherDTO = TopTeacherDTO.builder()
-//                        .teacherId(activeTeacher.getId())
-//                        .firstName(activeTeacher.getFirstname())
-//                        .lastName(activeTeacher.getLastname())
-//                        .phoneNumber(activeTeacher.getPhoneNumber())
-//                        .rating(sum/count)
-//                        .build();
-//                topTeacherMap.put(topTeacherDTO.getTeacherId(), topTeacherDTO.getRating());
-//                teacherList.add(topTeacherDTO);
-//            }
-//
-//            List<UUID> topTeacherList = topTeacherMap.entrySet().stream()
-//                    .sorted(Map.Entry.<UUID, Double>comparingByValue().reversed())
-//                    .limit(5)
-//                    .map(Map.Entry::getKey)
-//                    .toList();
-//
-//            List<TopTeacherDTO> topTeachers = new ArrayList<>();
-//            for (UUID topTeacher : topTeacherList) {
-//                for (TopTeacherDTO teachers : teacherList) {
-//                    if (teachers.getTeacherId()==topTeacher){
-//                        topTeachers.add(teachers);
-//                        break;
-//                    }
-//                }
-//            }
-//            return new ApiResponse("Success", true, HttpStatus.OK, topTeachers);
-//        }
-//        return new ApiResponse("No active teachers found", false, HttpStatus.BAD_REQUEST, null);
-//    }
-
-
-
-//    Student
 
 
 }
