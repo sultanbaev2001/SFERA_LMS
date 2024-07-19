80d4725f9e0fb03bfc4199606d7dcce8b36cfb71
Murad <sultanbaevm2001@gmail.com>
2024-07-19 23:00:52 +0500

    Student tayyor

--- src/main/java/sfera/repository/TaskRepository.java
+++ src/main/java/sfera/repository/TaskRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.repository.query.Param;
 import sfera.entity.Task;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
 
     @Query(value ="select * from lesson_task_list as lt inner join task as t on lt.lesson_id=t.id where lt.lesson_id=:lessonId" ,nativeQuery = true)
     List<Task> getAllLessonTasks(@Param("lessonId") Integer lessonId);
+
+
+    List<Task> getOpenTasks(@Param("userId")UUID userId);
 }
