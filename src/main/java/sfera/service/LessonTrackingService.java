c0236a1bc7662a39d3c1fba5031f2ad0a0ab3f95
Ulugbek Bakhtiyorov <164327420+bakhtiyorov-306@users.noreply.github.com>
2024-07-25 10:59:53 +0500

    LessonTrackingService

--- src/main/java/sfera/service/LessonTrackingService.java
+++ src/main/java/sfera/service/LessonTrackingService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import sfera.entity.Group;
+import sfera.entity.Lesson;
 import sfera.entity.LessonTracking;
 import sfera.entity.User;
 import sfera.exception.GenericException;
@@ -12,11 +13,11 @@ import sfera.payload.ApiResponse;
 import sfera.payload.req.ReqLessonTracking;
 import sfera.payload.res.ResLessonTracking;
 import sfera.repository.GroupRepository;
+import sfera.repository.LessonRepository;
 import sfera.repository.LessonTrackingRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +26,7 @@ public class LessonTrackingService {
     private final LessonTrackingRepository lessonTrackingRepository;
     private final LessonTrackingMapper lessonTrackingMapper;
     private final GroupRepository groupRepository;
+    private final LessonRepository lessonRepository;
 
 
     public ApiResponse saveLessonTracking(ReqLessonTracking reqLessonTracking, User user) {
@@ -68,15 +70,19 @@ public class LessonTrackingService {
     }
 
     public ApiResponse updateLessonTracking(Integer id, ReqLessonTracking reqLessonTracking, User user) {
-        if (!lessonTrackingRepository.existsById(id)) {
-            LessonTracking lessonTracking = lessonTrackingRepository.findById(id).orElseThrow(() -> GenericException.builder()
-                    .message("LessonTracking not found")
-                    .statusCode(404)
-                    .build());
-            if (lessonTracking.getGroup().getTeacher().equals(user)){
-                lessonTrackingRepository.save(lessonTrackingMapper.toLessonTracking(reqLessonTracking));
+        LessonTracking lessonTrackingList = lessonTrackingRepository.findById(id).orElse(null);
+        if (lessonTrackingList ==null) {
+            Group group = groupRepository.findById(reqLessonTracking.getGroupId()).orElse(null);
+            Lesson lesson = lessonRepository.findById(reqLessonTracking.getLessonId()).orElse(null);
+            if (user == (group != null ? group.getTeacher() : null)){
+                LessonTracking lessonTracking = new LessonTracking();
+                lessonTracking.setId(id);
+                lessonTracking.setGroup(group);
+                lessonTracking.setLesson(lesson);
+                lessonTracking.setActive(reqLessonTracking.isActive());
                 return new ApiResponse("Success", true, HttpStatus.OK, null);
             }
+            return new ApiResponse("Group does not belong to the teacher!", false, HttpStatus.BAD_REQUEST, null);
         }
         return new ApiResponse("LessonTracking not found", false, HttpStatus.BAD_REQUEST, null);
     }
@@ -84,5 +90,10 @@ public class LessonTrackingService {
     public ApiResponse deleteLessonTracking(Integer id) {
         lessonTrackingRepository.deleteById(id);
         return new ApiResponse("Success", true, HttpStatus.OK, null);
+    }
+
+
+    public void updateLesTrac(Integer id, ReqLessonTracking reqLessonTracking){
+
     }
 }
